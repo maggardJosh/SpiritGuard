7f762e957bc121f5d7c1fbda285fbf0d31c0b130
Josh Maggard <jmaggard62@gmail.com>
2015-08-18 16:15:42 -0500

    Hit switch sound effect

--- Assets/Resources/Scripts/GameObjects/HitSwitch.cs
+++ Assets/Resources/Scripts/GameObjects/HitSwitch.cs
@@ -11,22 +11,24 @@ public class HitSwitch : FutilePlatformerBaseObject
     public HitSwitch(string doorName, World world)
         : base(new RXRect(0, 0, 10,10), world)
     {
+        this.handleStateCount = true;
         this.blocksOtherObjects = true;
         this.doorName = doorName;
         sprite = new FAnimatedSprite("HitSwitch/switch");
         sprite.addAnimation(new FAnimation("idle", new int[] { 1 }, 100, false));
-        sprite.addAnimation(new FAnimation("activate", new int[] { 1, 2, 3 }, 150, false));
+        sprite.addAnimation(new FAnimation("activate", new int[] { 1,1,1,1,2, 3 }, 150, false));
         sprite.play("idle");
         sprite.ignoreTransitioning = true;
         this.AddChild(sprite);
     }
-
     public void Activate()
     {
+        if (stateCount < .5f)
+            return;
+        stateCount = 0;
+        FSoundManager.PlaySound("hitSwitch");
         if (pressed)
             return;
-
-        FSoundManager.PlaySound("hitSwitch");
         sprite.play("activate", true);
         C.getCameraInstance().shake(.7f, .3f);
         pressed = true;
@@ -36,7 +38,7 @@ public class HitSwitch : FutilePlatformerBaseObject
     }
     public void HandlePlayerCollision(Player p)
     {
-        if (pressed)
+        if (stateCount < .5f)
             return;
         if (p.shouldDamage && p.swordCollision.isColliding(this))
             Activate();
