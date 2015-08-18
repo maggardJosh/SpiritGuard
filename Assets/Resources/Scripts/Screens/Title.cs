bffe4281819bb71df8babdbb053cf637fd7a5995
Josh Maggard <jmaggard62@gmail.com>
2015-08-18 15:04:27 -0500

    Press l to start

--- Assets/Resources/Scripts/Screens/Title.cs
+++ Assets/Resources/Scripts/Screens/Title.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public class TitleScreen : FContainer
 {
     FSprite title;
     FSprite english;
     FSprite play;
-    FSprite exit;
     FSprite selected;
     FSprite credits;
     FSprite white;
@@ -18,8 +18,7 @@ public class TitleScreen : FContainer
         FSoundManager.PlayMusic("SpiritGuardMenu");
         title = new FSprite("title_kanji");
         english = new FSprite("title_english");
-        play = new FSprite("button_start");
-        exit = new FSprite("button_exit");
+        play = new FSprite("pressLToStart");
         credits = new FSprite("credits");
         white = new FSprite("white");
         white.width = Futile.screen.width;
@@ -28,7 +27,6 @@ public class TitleScreen : FContainer
         this.AddChild(title);
         this.AddChild(english);
         this.AddChild(play);
-        this.AddChild(exit);
         this.AddChild(credits);
 
         FLabel test = new FLabel(C.smallFontName, C.versionNumber);
@@ -47,28 +45,33 @@ public class TitleScreen : FContainer
         title.y = Futile.screen.halfHeight + title.height / 2f;
         english.x = Futile.screen.halfWidth + english.width / 2f;
         play.x = Futile.screen.halfWidth + play.width / 2f;
-        exit.x = Futile.screen.halfWidth + exit.width / 2f;
         credits.y = -Futile.screen.halfHeight - credits.height / 2f - 30f;
         Go.to(credits, 2.0f, new TweenConfig().floatProp("y", -Futile.screen.halfHeight + credits.height + 1).setEaseType(EaseType.BackOut).setDelay(3.0f));
         Go.to(title, 1.5f, new TweenConfig().floatProp("y", Futile.screen.halfHeight / 2f).setEaseType(EaseType.BounceOut).onComplete(() =>
         {
             english.y = title.y - title.height / 2f - english.height / 2f - 4;
-            play.y = english.y - english.height / 2f - play.height / 2f - 15;
-            exit.y = play.y - play.height / 2f - exit.height / 2f - 8f;
+            play.y = english.y - english.height / 2f - play.height / 2f - 20;
             Go.to(english, 2.0f, new TweenConfig().floatProp("x", 0).setEaseType(EaseType.BackOut).onComplete(() =>
             {
                 Go.to(title, 2.0f, new TweenConfig().floatProp("y", 2, true).setEaseType(EaseType.BackIn).setIterations(-1, LoopType.PingPong));
+                
                 //     Go.to(english, 2.0f, new TweenConfig().floatProp("y", 4, true).setEaseType(EaseType.QuadIn).setIterations(-1, LoopType.PingPong));
 
             }));
-            Go.to(play, 2.0f, new TweenConfig().floatProp("x", 0).setDelay(.5f).setEaseType(EaseType.BackOut));
-            Go.to(exit, 2.0f, new TweenConfig().floatProp("x", 0).setDelay(.7f).setEaseType(EaseType.BackOut));
+            Go.to(play, 2.0f, new TweenConfig().floatProp("x", 0).setDelay(.5f).setEaseType(EaseType.BackOut).onComplete(() => {
+                isTransIn = true;
+                //Go.to(play , 1.0f, new TweenConfig().floatProp("y", 2, true).setEaseType(EaseType.QuadIn).setIterations(-1, LoopType.PingPong));
+            }));
         }));
                 Futile.instance.SignalUpdate += Update;
         base.HandleAddedToStage();
     }
+    float count = 0;
+    bool isTransIn = false;
     public void Update()
     {
+        if(isTransIn)
+        count += Time.deltaTime;
         if (C.getKeyDown(C.JUMP_KEY))
         {
             World world = new World();
@@ -86,6 +89,7 @@ public class TitleScreen : FContainer
             });
             Futile.instance.SignalUpdate -= Update;
         }
+        this.play.isVisible = (count * 1500 % 2000 < 1500 );
     }
 }
 
